3457159cb68953d2e51898d08a07fe8468d37f03
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-01-24 02:46:51 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,11 @@
 #write your code here
 
-number = 10 
-
-def countdown
-  while number < 1
+def countdown(number)
+  while number > 0
   puts "#{number} SECONDS(S)!"
   number -= 1
 end
-end
 "HAPPY NEW YEAR!"
+end
+
 
